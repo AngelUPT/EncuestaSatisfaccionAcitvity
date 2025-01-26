efa459b30fb11174e54cccfafed599ffd171d355
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 14:12:53 -0600

    adding actions

--- app/src/main/java/com/angelcorrea/encuestasatisfaccionactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/encuestasatisfaccionactivity/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.angelcorrea.encuestasatisfaccionactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val edtNombre = findViewById<EditText>(R.id.edtNombre)
+        val edtComentarios = findViewById<EditText>(R.id.edtComentarios)
+        val seekBarSatisfaccion = findViewById<SeekBar>(R.id.seekBarSatisfaccion)
+        val txtValorSatisfaccion = findViewById<TextView>(R.id.txtValorSatisfaccion)
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+
+        seekBarSatisfaccion.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                txtValorSatisfaccion.text = "Valor: $progress"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        btnEnviar.setOnClickListener {
+            val nombre = edtNombre.text.toString()
+            val comentarios = edtComentarios.text.toString()
+            val satisfaccion = seekBarSatisfaccion.progress
+
+            if (nombre.isNotEmpty() && comentarios.isNotEmpty()) {
+                val message = "Nombre: $nombre\n" +
+                        "Nivel de Satisfacción: $satisfaccion\n" +
+                        "Comentarios: $comentarios"
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Complete todos los campos.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
